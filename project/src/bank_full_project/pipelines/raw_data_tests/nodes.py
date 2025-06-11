262602c73948ebae11399e61a7464823ee524f0f
Vilma Norja <vilma.norja@aalto.fi>
2025-06-11 17:32:14 +0100

    edit data unit tests

--- project/src/bank_full_project/pipelines/raw_data_tests/nodes.py
+++ project/src/bank_full_project/pipelines/raw_data_tests/nodes.py
@@ -94,112 +94,18 @@ def build_expectation_suite(expectation_suite_name: str, feature_group: str) -> 
                 )
             )
 
-    # categorical features
     if feature_group == 'categorical_features':
-
-        expectation_suite_bank.add_expectation(
-            ExpectationConfiguration(
-                expectation_type="expect_column_distinct_values_to_be_in_set",
-                kwargs={"column": "customer_status", "value_set": ['Private Client - Standard', 'Corporate - Small', 'Corporate - Medium', 'Private Client High Networth', 'Corporate - Large', 'Financial - Large', 'Financial - Small', 'Proprietorship Standard', 'T24 Updates', 'Deceased Individual', 'Partnership firm Standard', 'Financial - Medium', 'Partnership High Networth', 'Customer Deletion', 'Governmental', 'Hotlisted', 'Proprietorship Highnetworth']},
-            )
-        ) 
-        expectation_suite_bank.add_expectation(
-            ExpectationConfiguration(
-                expectation_type="expect_column_distinct_values_to_be_in_set",
-                kwargs={"column": "employment_status", "value_set": ['MB', 'LP', 'OTHER', 'EMPLOYED', 'TPE', 'UNEMPLOYED', 'SELF-EMPLOYED', 'RET', 'UE', 'STUDENT', 'RETIRED']},
-            )
-        ) 
-        expectation_suite_bank.add_expectation(
-            ExpectationConfiguration(
-                expectation_type="expect_column_distinct_values_to_be_in_set",
-                kwargs={"column": "gender", "value_set": ['FEMALE', 'MALE']},
-            )
-        ) 
-        expectation_suite_bank.add_expectation(
-            ExpectationConfiguration(
-                expectation_type="expect_column_distinct_values_to_be_in_set",
-                kwargs={"column": "marital_status", "value_set": ['OTHER', 'DIVORCED', 'SINGLE', 'MARRIED', 'PARTNER', 'WIDOWED']},
-            )
-        ) 
-        expectation_suite_bank.add_expectation(
-            ExpectationConfiguration(
-                expectation_type="expect_column_distinct_values_to_be_in_set",
-                kwargs={"column": "cust_type", "value_set": ['RETAIL', 'CORPORATE', 'PROSPECT']},
-            )
-        ) 
-        expectation_suite_bank.add_expectation(
-            ExpectationConfiguration(
-                expectation_type="expect_column_distinct_values_to_be_in_set",
-                kwargs={"column": "nationality", "value_set": ['Portugal', 'Mozambique', 'India', 'Pakistan', 'Peoples Republic of China', 'Tokelau', 'South Africa', 'Turkey', 'Netherlands', 'Egypt', 'Switzerland', 'United States of America', 'Congo', 'Yemen', 'Uruguay', 'Afghanistan', 'Korea  Republic of', 'Australia', 'Morocco', 'Somalia', 'Spain', 'Bahrain', 'Lebanon', 'Kenya', 'Mauritius', 'Tanzania  United Republic of', 'Ethiopia', 'Guinea-Bissau', 'Mali', 'Mauritania', 'Guinea', 'Gambia', 'Sierra Leone', 'Mexico', 'Bangladesh', 'Aruba', 'United Arab Emirates', 'Zambia', 'Saudi Arabia', 'Singapore', 'Iran (Islamic Republic of)', 'Nigeria', 'Angola', 'Rwanda', 'Zimbabwe', 'Senegal', 'Hong Kong', 'New Zealand', 'Brazil', 'Italy', 'Cape Verde', 'Philippines', 'Malta', 'Oman', 'Jordan', 'Syrian Arab Republic', 'Libyan Arab Jamahiriya', 'Estado da Palestina', 'Malawi', 'Iraq', 'Great Britain', 'Turks and Caicos Islands', 'Germany', 'Japan', 'Namibia', 'Chile', 'Swaziland', 'Uzbekistan', 'France', 'Luxembourg', 'Norway', 'Bulgaria', 'Croatia', 'Canada', 'Ireland', 'Russian Federation', 'Burundi', 'Czech Republic', 'Uganda', 'Liberia', 'Poland', 'Belgium', 'Benin', 'Greece', 'Kyrgyzstan', 'American Samoa', 'Sao Tome and Principe', 'Tunisia', 'Haiti', 'Columbia', 'Macau', 'Romania', 'Cuba', 'Peru', 'Republic of China (Taiwan)', 'Kazakstan', 'Ghana', 'Finland', 'Puerto Rico', 'Vietnam', 'Israel', 'Thailand', 'Latvia', 'Sri Lanka', 'Nicaragua', 'Congo  Democratic Republic of the', 'Panama', 'Austria', 'Botswana', 'Indonesia', 'Madagascar', 'Comoro Islands', 'Cameroon', 'Nepal', 'Bhutan', 'Sudan', 'Sweden', 'Serbia', 'Mongolia', 'Paraguay', 'Argentina', 'Malaysia', 'Cyprus', 'Honduras', 'Guatemala', 'Monaco', 'Gabon', 'Denmark', 'Eritrea', 'Armenia', 'Ecuador', 'EU Countries', 'Venezuela', 'Dominica', 'Slovenia', 'Antigua And Barbuda', 'Lesotho', 'Georgia', 'Ivory Coast', 'Europa', 'Timor-leste', 'Cayman Islands', 'Northern Mariana Islands', 'San Marino', 'Dominican Republic', 'Chad', 'Algeria', 'Korea  Democratic Peoples Rep. of', 'Iceland', 'Turkmenistan', 'Ukraine', 'Bahamas', 'Bolivia', 'Niger', 'Liechtenstein', 'Hungary', 'Monserrat', 'Jersey', 'Estonia', 'Guadeloupe', 'Costa Rica', 'Lithuania', 'Guyana', 'Qatar', 'Andorra', 'Reunion', 'Yugoslavia', 'Belarus', 'Slovakia', 'Saint Lucia', 'Papua New Guinea', 'Barbados', 'Azerbaijan', 'Fiji', 'Holy See (Vatican City State)', 'Central African Republic']},
-            )
-        ) 
-        expectation_suite_bank.add_expectation(
-            ExpectationConfiguration(
-                expectation_type="expect_column_distinct_values_to_be_in_set",
-                kwargs={"column": "residence_code", "value_set": ['MZ', 'PT', 'ZA', 'NL', 'EG', 'CH', 'CN', 'US', 'IN', 'YE', 'UY', 'AF', 'KR', 'AU', 'ES', 'BH', 'LB', 'MU', 'TZ', 'KE', 'AW', 'AE', 'TR', 'AO', 'NG', 'ZM', 'RW', 'BR', 'ZW', 'MA', 'MR', 'IT', 'NZ', 'MT', 'OM', 'JP', 'CL', 'SZ', 'SA', 'FR', 'LU', 'HR', 'PL', 'GR', 'KG', 'ST', 'TN', 'IR', 'IE', 'GB', 'MW', 'VN', 'JO', 'FI', 'SE', 'PK', 'LV', 'BE', 'CV', 'CU', 'PA', 'MX', 'CO', 'DE', 'AT', 'BW', 'ID', 'MG', 'CA', 'IQ', 'HK', 'KM', 'CM', 'NO', 'CD', 'SG', 'RO', 'RS', 'GH', 'PY', 'PH', 'TW', 'LS', 'MY', 'BM', 'CZ', 'MC', 'AR', 'DK', 'PE', 'XE', 'SI', 'AG', 'TH', 'UG', 'RE', 'GE', 'GM', 'CI', 'KZ', 'CY', 'GA', 'GN', 'MO', 'KP', 'TC', 'IL', 'MN', 'BS', 'UA', 'RU', 'MP', 'HU', 'LR', 'EE', 'AM', 'CR', 'BD', 'SO', 'AD', 'SN', 'SK', 'LC', 'AS', 'BB', 'LT', 'BI', 'LK', 'ML', 'ET', 'EU', 'ER']},
-            )
-        ) 
-        expectation_suite_bank.add_expectation(
-            ExpectationConfiguration(
-                expectation_type="expect_column_distinct_values_to_be_in_set",
-                kwargs={"column": "residence_status", "value_set": ['HOME.OWNER', 'OTHER', 'TENANT', 'LIVING.WTH.PARENTS', 'SQUATTER']},
-            )
-        ) 
-        expectation_suite_bank.add_expectation(
-            ExpectationConfiguration(
-                expectation_type="expect_column_distinct_values_to_be_in_set",
-                kwargs={"column": "residence_type", "value_set": ['RESIDENTIAL.APT', 'INDEPEDENT.HOUSE', 'FARM.HOUSE', 'SERVICED.APT']},
-            )
-        ) 
-        expectation_suite_bank.add_expectation(
-            ExpectationConfiguration(
-                expectation_type="expect_column_distinct_values_to_be_in_set",
-                kwargs={"column": "seg_group", "value_set": ['Company', 'Personal']},
-            )
-        ) 
-        expectation_suite_bank.add_expectation(
-            ExpectationConfiguration(
-                expectation_type="expect_column_distinct_values_to_be_in_set",
-                kwargs={"column": "title", "value_set": ['MRS', 'MR', 'DR', 'DRS', 'MISS', 'MAST1', 'ENG', 'PHD', 'MAST']},
-            )
-        ) 
-        expectation_suite_bank.add_expectation(
-            ExpectationConfiguration(
-                expectation_type="expect_column_distinct_values_to_be_in_set",
-                kwargs={"column": "cust_type1", "value_set": ['RETAIL', 'CORPORATE', 'PROSPECT']},
-            )
-        ) 
-        expectation_suite_bank.add_expectation(
-            ExpectationConfiguration(
-                expectation_type="expect_column_distinct_values_to_be_in_set",
-                kwargs={"column": "habliter", "value_set": ['Ate12 ano', 'Curso Superior', 'Bacharelato', 'Doutoramento', 'Mestrado', 'S.Estudos', 'Ensino Primario']},
-            )
-        ) 
-        expectation_suite_bank.add_expectation(
-            ExpectationConfiguration(
-                expectation_type="expect_column_distinct_values_to_be_in_set",
-                kwargs={"column": "province", "value_set": ['NAMPULA', 'CIDADE DE MAPUTO', 'MAPUTO', 'SOFALA', 'TETE', 'CABO DELGADO', 'NOT APPLICABLE', 'MANICA', 'ZAMBEZIA', 'NIASSA', 'INHAMBANE', 'GAZA']},
-            )
-        ) 
-        expectation_suite_bank.add_expectation(
-            ExpectationConfiguration(
-                expectation_type="expect_column_distinct_values_to_be_in_set",
-                kwargs={"column": "legal_doc_name1_id_description", "value_set": ['Licenca/Alvara', 'DIRE', 'Certidao da Conservatoria de Regist', 'Licenca / Alvara para o exercicio d', 'BI', 'Certidao de reserva de nome', 'Passaporte', 'Others (specify on free text field)', 'Certidao de Registo Comercial (Caso', 'Acta(s) da Assembleia Geral dos Acc', 'Escritura Publica da constituicao d', 'Certidao de Registo / Ministerio da', 'NUIT- Numero Unico de Identificacao', 'Cedula Pessoal', 'Recibo de pedido de BI', 'Cartao de identificacao de refugiad', 'NUIT-Numero Unico de Identificac', 'Carta de conducao', 'BR com o Pacto Social / Estatutos d', 'Termo de Autorizacao do Ministerio"', 'Certidao narrativa completa de nasc', 'Certidao da sentenca do Tribunal de', 'Numero de Registo Fiscal ( Modelo 4', 'Cartao de recenseamento eleitoral', 'Prova de Residencia', 'Prova documental de nao residente', 'Cartao de agricultor/trabalhador ab', 'Cartao de INSS', 'Documento de Transitario', 'Declaracao Comprovativa / Ministeri', 'Cedula militar', 'Documento de Regime Especial', 'Contrato de Trabalho', 'Documento de Embaixada', 'Procuracoes (especificar)', 'Documento de Staff Embaixada', 'Acta(s) da reuniao dos socios (pode', 'Documeto de Exportador']},
-            )
-        ) 
-        expectation_suite_bank.add_expectation(
-            ExpectationConfiguration(
-                expectation_type="expect_column_distinct_values_to_be_in_set",
-                kwargs={"column": "a_m_l_risk_rating", "value_set": ['Elevado', 'Medio', 'Baixo']},
-            )
-        ) 
-        for i in ['placebrth', 'ocupation_desc', 'town_country', 'district', 'legal_iss_auth', 'legal_exp_date', 'date_of_birth', 'birth_in_corp_date', 'legal_iss_date', 'customer_since']:
+        for i in ['customer_status','employment_status','gender','marital_status', 'placebrth', 'cust_type', 
+                  'nationality', 'ocupation_desc', 'residence_code','residence_status','residence_type',
+                  'seg_group','title', 'town_country', 'cust_type_1', 'habliter', 'province', 'district', 
+                  'legal_doc_name1_id_description', 'legal_iss_date', 'legal_iss_auth', 'a_m_l_risk_rating']:
             expectation_suite_bank.add_expectation(
                 ExpectationConfiguration(
-                    expectation_type="expect_column_values_to_be_of_type",
-                    kwargs={"column": i, "type_": "object"},
+                    expectation_type="expect_column_to_exist",
+                    kwargs={"column": i}
                 )
             )
+
     return expectation_suite_bank
 
 
