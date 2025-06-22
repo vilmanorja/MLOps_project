3f2f151611e94632542ac7faa11aedcb131f416a
Kida de Morais Aly <kidademoraisaly@gmail.com>
2025-06-22 19:02:52 +0100

    updating model predict full pipeline

--- src/mlops_credit_scoring/pipelines/model_train/nodes.py
+++ src/mlops_credit_scoring/pipelines/model_train/nodes.py
@@ -90,13 +90,12 @@ def model_train(X_train: pd.DataFrame,
         logger.info(f"F1-Score is {f1_test}")
         logger.info(f"Recall is {recall_test}")
         
-        #logging the model
+        #logging the model and registering the model
         mlflow.sklearn.log_model(
             model,
             artifact_path="model",
             registered_model_name="loan_default_model"
         )
-       
         model_uri = f"runs:/{run_id}/model"
         result = mlflow.register_model(model_uri, "loan_default_model")
 
